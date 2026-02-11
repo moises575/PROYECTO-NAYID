6520aca3d2f58b359c2f76fce69f82e6533d3b44
moises575 <morus.x04@gmail.com>
2026-02-11 02:25:35 +0000

    prueba1

---
+++ ProyectoN.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+import pandas as pd
+
+
+
+
+st.title("Análisis de Horas Pico, Vehículos y Viajes Completados en la App Uber")
+
+st.title("______________________________")
+
