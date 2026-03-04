9568439b0823dd042defcb6f7d80716db4c4f471
moises575 <morus.x04@gmail.com>
2026-03-04 01:00:25 +0000

    cambio de posicion de filtro

--- ProyectoN_moi.py
+++ ProyectoN_moi.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-st.title("GRAFICO DE TIPOS DE VEICULOS UTILISADOS POR LOS CHOFERES")
-st.caption("Moisés Porras Valverde")
+st.set_page_config(layout="wide")
 
-st.title("FINTRO DE AUTOS MAS USADOS POR LOS CHOFERES")
+st.title("GRAFICO DE TIPOS DE VEHICULOS UTILIZADOS POR LOS CHOFERES")
+st.caption("Moisés Porras Valverde")
 
 df = pd.read_csv("ncr_ride_bookings.csv")
 df.columns = df.columns.str.strip()
 
+st.sidebar.title("Filtros")
+
 opciones = df["Booking Status"].dropna().unique()
 
-seleccion = st.multiselect(
+seleccion = st.sidebar.multiselect(
     "Selecciona Booking Status:",
     options=opciones,
     default=opciones
@@ -19,14 +21,14 @@ seleccion = st.multiselect(
 
 df_filtrado = df[df["Booking Status"].isin(seleccion)]
 
-st.write("Cantidad de registros:", df_filtrado.shape[0])
+st.subheader("Cantidad de registros")
+st.write(df_filtrado.shape[0])
 
 with st.expander("Ver tabla de datos"):
     st.dataframe(df_filtrado)
 
-st.title("TIPO DE AUTOS MAS USADOS POR LOS CHOFERES")
+st.subheader("TIPO DE AUTOS MAS USADOS POR LOS CHOFERES")
 
 conteo = df_filtrado["Vehicle Type"].value_counts()
 
 st.bar_chart(conteo)
-
