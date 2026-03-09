b4b9ee1baced7f5fd56ddd4677d7d95b44b174f4
moises575 <morus.x04@gmail.com>
2026-03-09 17:02:56 +0000

    mejorado

--- ProyectoN_manuel.py
+++ ProyectoN_manuel.py
@@ -1,15 +1,28 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("ncr_ride_bookings.csv")
-df.columns = df.columns.str.strip()
-
 st.subheader("Estado de los Viajes")
 
-status_count = df["Booking Status"].value_counts()
+status_count = filtered_df['booking_status'].value_counts()
+
 status_percent = (status_count / status_count.sum()) * 100
+
+status_count.index = status_count.index.map({
+    "Completed": "Completado",
+    "Incomplete": "Incompleto",
+    "Canceled": "Cancelado"
+})
+
+status_percent.index = status_percent.index.map({
+    "Completed": "Completado",
+    "Incomplete": "Incompleto",
+    "Canceled": "Cancelado"
+})
 
 st.bar_chart(status_count)
 
-st.write("Porcentaje de cada estado:")
-st.write(status_percent.round(2))
+tabla_porcentaje = status_percent.round(2).reset_index()
+tabla_porcentaje.columns = ["Estado del Viaje", "Porcentaje (%)"]
+
+st.write("### Porcentaje de cada estado")
+st.dataframe(tabla_porcentaje)
